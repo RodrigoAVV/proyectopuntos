bd8f094bcd072045042cb43efa55e1834406de22
RodrigoVV <rodrigo.vidal@virginiogomez.cl>
2022-06-03 08:46:19 -0400

    Se asignan tareas

---
+++ src/main/java/cl/lec/ProyectoRama.java
@@ -0,0 +1,31 @@
+package cl.lec;
+public class ProyectoRama {
+    //Leonardo
+    public static void mostrarPuntos(int puntos[]){
+        
+    }
+    //Wilgen
+    public static void buscarPunto(int puntos[],int buscar){
+        
+    }
+    //Constanza
+    public static void puntosMayores50(int puntos[]){
+        
+    }
+    //Alejandro
+    public static void puntosMenoresIguales50(){
+        
+    }
+    //Maria
+    public static float promedioPuntos(int puntos[]){
+        
+        return 0;
+    }
+    //Keshny
+    public static void mostrarPuntosInverso(int puntos[]){
+        
+    }
+    public static void main(String[] args) {
+        int puntos[] = {10,20,30,40,50,60,70,80,90,100};
+    }
+}
